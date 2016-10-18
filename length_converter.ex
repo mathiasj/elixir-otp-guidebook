d4a8b35f36d2aada9e49f9b6785b079af5fd3d81
Mathias Johansson <hej@mathiasjohansson.se>
2016-10-19 00:28:00 +0200

    Listing 2.3

--- length_converter.ex
+++ length_converter.ex
@@ -1,13 +1,11 @@
-defmodule MeterToLengthConverter do
-	defmodule Feet do
-		def convert(m) do
-			m * 3.28084
-		end
+defmodule MeterToLengthConverter.Feet do
+	def convert(m) do
+		m * 3.28084
 	end
+end
 
-	defmodule Inch do 
-		def convert(m) do
-			m * 39.3701
-		end
+defmodule MeterToLengthConverter.Inch do 
+	def convert(m) do
+		m * 39.3701
 	end
 end
